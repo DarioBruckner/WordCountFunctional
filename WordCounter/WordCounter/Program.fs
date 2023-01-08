33b746be367b7dc69fcac2711f60d952c03c5e3e
DarioBruckner <if20b023@technikum-wien.at>
2023-01-08 15:46:30 +0100

    Added Multiple files function

--- WordCounter/WordCounter/Program.fs
+++ WordCounter/WordCounter/Program.fs
@@ -1,22 +1,43 @@
 ﻿open System
 open System.IO
+open FSharp.Collections.ParallelSeq
 
-
-let input = File.ReadAllText("..\..\..\..\Text.txt")
-
+let GetAllFiles (location : string) =
+    (Directory.GetFiles(location, "*.txt"))
+   
 
 let count (text : string) =
     let words = text.Split([|'.'; ' '; '\n'; '\r'; '*';'+';':'; '\"'; '('; ')';'['; ']'; '#'; ','; '-';';';'!';'?';':'; '\''; '\\'; '/'; '&'; '%'; '$'; '<';'>'; '`'; '^'; '@';'_';'='; '~';'{';'|';'}'|]) 
     (words)
+   
+let AddallFilesToString (files : string[]) =
+    let mutable temp = ""
+    let mutable listofStrings : string list = []
+    
+    
+    for item in files do
+        temp <- File.ReadAllText(item)
+        listofStrings <- temp :: listofStrings
+  
+    let ret = String.concat " " listofStrings
+        
+    (ret)
+
+
+let userdirectory = System.Console.ReadLine();
+
+let arr = GetAllFiles userdirectory
+
+let input = AddallFilesToString arr
 
 let temp = count input
 
 let output =
     temp
-    |> Seq.sort 
-    |> Seq.countBy id
-    |> Seq.toList
-    |> List.sortByDescending snd
+    |> PSeq.sort
+    |> PSeq.countBy id
+    |> PSeq.ordered
+    |> PSeq.toList
 
 for item in output do
     printfn "%A %A" (snd item) (fst item)
